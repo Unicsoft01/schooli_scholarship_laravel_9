35c71d2f8ae96999a983143a74492b33fa8211ec
Yakub Muhammed <yakubmuhammed51@gmail.com>
2022-12-10 17:46:50 +0100

    setup themes and route user dashboard

---
+++ resources/views/users/dashboard.blade.php
@@ -0,0 +1,83 @@
+@php
+  $site = App\Models\Settings::find(1);
+@endphp
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <base href="{{ url('/') }}">
+    <!-- Twitter -->
+    <meta name="twitter:site" content="@themepixels">
+    <meta name="twitter:creator" content="@themepixels">
+    <meta name="twitter:card" content="summary_large_image">
+    <meta name="twitter:title" content="Starlight">
+    <meta name="twitter:description" content="Premium Quality and Responsive UI for Dashboard.">
+    <meta name="twitter:image" content="http://themepixels.me/starlight/img/starlight-social.png">
+
+    <!-- Facebook -->
+    <meta property="og:url" content="http://themepixels.me/starlight">
+    <meta property="og:title" content="Starlight">
+    <meta property="og:description" content="Premium Quality and Responsive UI for Dashboard.">
+
+    <meta property="og:image" content="http://themepixels.me/starlight/img/starlight-social.png">
+    <meta property="og:image:secure_url" content="http://themepixels.me/starlight/img/starlight-social.png">
+    <meta property="og:image:type" content="image/png">
+    <meta property="og:image:width" content="1200">
+    <meta property="og:image:height" content="600">
+
+    <!-- Meta -->
+    <meta name="description" content="Premium Quality and Responsive UI for Dashboard.">
+    <meta name="author" content="ThemePixels">
+
+    <title>{{ $pageTitle }} - {{ $site->site_name }}</title>
+
+    <!-- vendor css -->
+    <link href="{{ url('/') }}/lib/font-awesome/css/font-awesome.css" rel="stylesheet">
+    <link href="{{ url('/') }}/lib/Ionicons/css/ionicons.css" rel="stylesheet">
+    <link href="{{ url('/') }}/lib/perfect-scrollbar/css/perfect-scrollbar.css" rel="stylesheet">
+    <link href="{{ url('/') }}/lib/rickshaw/rickshaw.min.css" rel="stylesheet">
+
+    <!-- Starlight CSS -->
+    <link rel="stylesheet" href="{{ url('/') }}/css/starlight.css">
+  </head>
+
+  <body>
+
+    <!-- ########## START: LEFT PANEL ########## -->
+    @include('users.inc.lsidebar')
+    <!-- ########## END: LEFT PANEL ########## -->
+
+    <!-- ########## START: HEAD PANEL ########## -->
+    @include('users.inc.header')
+    <!-- ########## END: HEAD PANEL ########## -->
+
+    <!-- ########## START: RIGHT PANEL ########## -->
+    @include('users.inc.rsidebar')
+    <!-- ########## END: RIGHT PANEL ########## --->
+
+    <!-- ########## START: MAIN PANEL ########## -->
+    @yield('main_contents')
+    <!-- sl-mainpanel -->
+    <!-- ########## END: MAIN PANEL ########## -->
+
+    <script src="{{ url('/') }}/lib/jquery/jquery.js"></script>
+    <script src="{{ url('/') }}/lib/popper.js/popper.js"></script>
+    <script src="{{ url('/') }}/lib/bootstrap/bootstrap.js"></script>
+    <script src="{{ url('/') }}/lib/jquery-ui/jquery-ui.js"></script>
+    <script src="{{ url('/') }}/lib/perfect-scrollbar/js/perfect-scrollbar.jquery.js"></script>
+    <script src="{{ url('/') }}/lib/jquery.sparkline.bower/jquery.sparkline.min.js"></script>
+    <script src="{{ url('/') }}/lib/d3/d3.js"></script>
+    <script src="{{ url('/') }}/lib/rickshaw/rickshaw.min.js"></script>
+    <script src="{{ url('/') }}/lib/chart.js/Chart.js"></script>
+    <script src="{{ url('/') }}/lib/Flot/jquery.flot.js"></script>
+    <script src="{{ url('/') }}/lib/Flot/jquery.flot.pie.js"></script>
+    <script src="{{ url('/') }}/lib/Flot/jquery.flot.resize.js"></script>
+    <script src="{{ url('/') }}/lib/flot-spline/jquery.flot.spline.js"></script>
+
+    <script src="{{ url('/') }}/js/starlight.js"></script>
+    <script src="{{ url('/') }}/js/ResizeSensor.js"></script>
+    <script src="{{ url('/') }}/js/dashboard.js"></script>
+  </body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,14 +19,26 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+// Route::get('/dashboard', function () {
+//     return view('dashboard');
+// })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+Route::middleware(['auth', 'verified'])->group(function () {
+
+    Route::controller(UserController::class)->group(function (){
+        Route::get('/dashboard', 'index')->name('dashboard');
+        // Route::post('/contact/Store', 'ContactStore')->name('contact.store');
+    });
+
+
+
+});
+
 
 require __DIR__.'/auth.php';
